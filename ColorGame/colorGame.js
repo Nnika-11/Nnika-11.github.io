252edf801ad3d9b695f9fa1c40accd11d181a0c2
Nnika-11 <sherr.veronika@gmail.com>
2020-09-23 09:55:20 -0400

    random colors generator

--- ColorGame/colorGame.js
+++ ColorGame/colorGame.js
@@ -1,16 +1,10 @@
 // alert("connected");
-var colors = [
-  "rgb(255, 0, 0)",
-  "rgb(255, 255, 0)",
-  "rgb(0, 255, 0)",
-  "rgb(0, 255, 255)",
-  "rgb(0, 0, 255)",
-  "rgb(255, 0, 255)"
-]
+var colors = generateColors(6);
 var squares = document.querySelectorAll(".square");
 var pickedColor = pickColor();
 var colorDisplay = document.getElementById("colorDisplay");
 var messageDisplay = document.querySelector("#message");
+var h1 = document.querySelector("h1");
 colorDisplay.textContent = pickedColor;
 for(var i = 0; i<squares.length; i++){
   //add initial colors to squates
@@ -30,6 +24,24 @@ for(var i = 0; i<squares.length; i++){
         }
   });
 }
+function generateColors(num){
+  //make an array with num random colors
+  var colorsArr =[];
+  for(var i=0; i<num; i++){
+    colorsArr.push(genRandomColor())
+  }
+  return colorsArr
+
+}
+function genRandomColor(){
+
+  //get num from 0 to 255
+  var redColor = Math.floor(Math.random()*256);
+  var redGreen = Math.floor(Math.random()*256);
+  var redBlue = Math.floor(Math.random()*256);
+  var randomColor ="rgb("+redColor +", "+ redGreen + ", " + redBlue +")"
+  return randomColor
+}
 
 function changeColors(){
   //loop throug all squares
@@ -37,14 +49,12 @@ function changeColors(){
     //change color to match picked one
     squares[i].style.backgroundColor = pickedColor;
   }
-  // h1.style.backgroundColor = pickedColor;
+  h1.style.backgroundColor = pickedColor;
 }
 function pickColor(){
-  //get num from 1 to 255
-  // Math.floor(Math.random()*255+1)
+
 
   // pick random from array
   var randomColor = Math.floor(Math.random()*colors.length);
   return colors[randomColor];
-
 }
