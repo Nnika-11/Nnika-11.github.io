596a5071e2a2b0cbf02f3a621d6860a9783f088b
Nnika-11 <sherr.veronika@gmail.com>
2020-09-22 21:08:52 -0400

    UX for interaction and responses

--- ColorGame/colorGame.js
+++ ColorGame/colorGame.js
@@ -10,16 +10,32 @@ var colors = [
 var squares = document.querySelectorAll(".square");
 var pickedColor = colors[3];
 var colorDisplay = document.getElementById("colorDisplay");
+var messageDisplay = document.querySelector("#message");
 colorDisplay.textContent = pickedColor;
 for(var i = 0; i<squares.length; i++){
   //add initial colors to squates
   squares[i].style.backgroundColor = colors[i];
-  //add click listeners to squares
   squares[i].addEventListener("click", function(){
-    //grab clicked color
-    var clickedColor = this.style.backgroundColor;
-    //compare color to pickedColor
-    if(clickedColor===pickedColor) alert("correct");
-    else alert("wrong");
+      //grab clicked color
+      var clickedColor = this.style.backgroundColor;
+        //compare color to pickedColor
+      if(clickedColor === pickedColor){
+        // (clickedColor===pickedColor) alert("correct");
+        messageDisplay.textContent = "Correct!"
+        changeColors();
+    }
+      else {
+          this.style.backgroundColor = "#232323";
+          messageDisplay.textContent = "Try again!"
+        }
   });
+}
+
+function changeColors(){
+  //loop throug all squares
+  for(var i=0; i<squares.length; i++){
+    //change color to match picked one
+    squares[i].style.backgroundColor = pickedColor;
+  }
+  // h1.style.backgroundColor = pickedColor;
 }
