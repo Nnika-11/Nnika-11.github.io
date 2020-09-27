fd81a448d23ac6e46712e95834032778772e6189
Nnika-11 <sherr.veronika@gmail.com>
2020-09-27 13:41:06 -0400

    add logic behind easy/hard mode

--- ColorGame/colorGame.js
+++ ColorGame/colorGame.js
@@ -1,5 +1,6 @@
 // alert("connected");
-var colors = generateColorsArray(6);
+var numSquares = 6;
+var colors = generateColorsArray(numSquares);
 var squares = document.querySelectorAll(".square");
 var pickedColor = pickColor();
 var colorDisplay = document.getElementById("colorDisplay");
@@ -11,9 +12,43 @@ var hardBtn = document.querySelector("#hardBtn");
 colorDisplay.textContent = pickedColor;
 
 
+easyBtn.addEventListener("click", function(){
+  easyBtn.classList.add("selected");
+  hardBtn.classList.remove("selected");
+  numSquares = 3;
+  colors = generateColorsArray(numSquares);
+  pickedColor = pickColor();
+  colorDisplay.textContent = pickedColor;
+  h1.style.backgroundColor = "initial";
+  for(var i = 0; i<squares.length; i++){
+    //check if there is a color at this index
+    if(colors[i])
+      squares[i].style.backgroundColor = colors[i];
+    else
+      squares[i].style.display = "none";
+
+  }
+});
+
+hardBtn.addEventListener("click", function(){
+  hardBtn.classList.add("selected");
+  easyBtn.classList.remove("selected");
+  numSquares = 6;
+  colors = generateColorsArray(numSquares);
+  pickedColor = pickColor();
+  colorDisplay.textContent = pickedColor;
+  h1.style.backgroundColor = "initial";
+  for(var i = 0; i<squares.length; i++){
+    //add initial colors to squates
+    squares[i].style.backgroundColor = colors[i];
+    squares[i].style.display = "block";
+  }
+
+});
+
 resetButton.addEventListener("click", function(){
     //change colors of squares
-  colors = generateColorsArray(6);
+  colors = generateColorsArray(numSquares);
   pickedColor = pickColor();
   //pick a random color
   resetButton.textContent = "New Colors";
