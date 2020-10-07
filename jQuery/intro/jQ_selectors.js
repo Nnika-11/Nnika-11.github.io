cc861433dac124a6245e5d100b2d37877a1ad08b
Nnika-11 <sherr.veronika@gmail.com>
2020-10-07 13:54:43 -0400

    jQuerrySelectors

---
+++ jQuery/intro/jQ_selectors.js
@@ -0,0 +1,4 @@
+$("h1").css("background-color", "yellow");
+$("h1").eq(1).before( "<h1> //added to a specific h1(1)</h1>").css("margin-top", "-20px");
+$("#c").append( "<span>. //added with id selector</span>" );
+$("li a").css("color", "red"); //all a inside li
