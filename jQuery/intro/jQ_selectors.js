8831ae51971139e3dd0c41004e1c45337c419fab
Nnika-11 <sherr.veronika@gmail.com>
2020-10-08 14:39:58 -0400

    JQuerry selectors

--- jQuery/intro/jQ_selectors.js
+++ jQuery/intro/jQ_selectors.js
@@ -1,4 +1,14 @@
+//dollar sign (selector).css(property, value);
+//dont require loop through to change style
 $("h1").css("background-color", "yellow");
-$("h1").eq(1).before( "<h1> //added to a specific h1(1)</h1>").css("margin-top", "-20px");
+//get 1-st el of h1 and add comment befor it while changing the parent
+$("h1").eq(1).before( "<h1> //added before the specific h1(1)</h1>").css("margin-top", "-20px");
 $("#c").append( "<span>. //added with id selector</span>" );
 $("li a").css("color", "red"); //all a inside li
+$("li:first-child").css("background-color", "white"); //first-of-type/first(slower)
+var styles = {
+  backgroundColor:"gray",
+  border: "2px solid black"
+}
+//selectAll by class name using obj styles
+$(".list").css(styles)
