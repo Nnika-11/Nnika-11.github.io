2576222f465afab4524f0e3a048e538e9441d4f6
Nnika-11 <sherr.veronika@gmail.com>
2020-10-08 14:49:30 -0400

    JQ methods example

---
+++ jQuery/intro/methods.js
@@ -0,0 +1,8 @@
+//get text of elements as one string
+$("li").text();
+//rewrite text of element
+$("h1").text("new way to change text with Methods");
+//get inner html as a one string
+$("li").html();
+//change inner html
+$("ul").html("<li>change 1</li><li>change 2</li>");
