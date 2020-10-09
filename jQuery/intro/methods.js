48bf0eec32eb942d30bfd43e40fa7848dec44661
Nnika-11 <sherr.veronika@gmail.com>
2020-10-09 15:24:57 -0400

    jsMethods intro

--- jQuery/intro/methods.js
+++ jQuery/intro/methods.js
@@ -6,3 +6,21 @@ $("h1").text("new way to change text with Methods");
 $("li").html();
 //change inner html
 $("ul").html("<li>change 1</li><li>change 2</li>");
+//some css
+$("img").css( "width","200px");
+//get arrtibute val with attr() method and change src
+$("img:first-of-type").attr("src","https://image.cnbcfm.com/api/v1/image/105828578-1554223245858gettyimages-149052633.jpeg?v=1554223281");
+$("img:last").attr("src","https://www.kaytee.com/-/media/images/kaytee-na/us/learn-care/ask-the-pet-bird-experts/ways-to-show-parrot-love/parrot%20png.png");
+//change input type to color
+$("input").attr("type","color");
+//extract value from input with val()
+$("input").val("#666477");
+//get val from select
+$("select").val(); //but cant write new one
+//working with classes
+//add classes to html
+$("h1").addClass("correct underlined");
+//remove class
+$("h1").removeClass("correct");
+//change state (add/remove)
+$("h1").toggleClass("correct");
