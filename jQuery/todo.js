4aec9c79b65eeaeb409154a514f784451d58621c
Nnika-11 <sherr.veronika@gmail.com>
2020-10-14 15:58:44 -0400

    todoJS

--- jQuery/todo.js
+++ jQuery/todo.js
@@ -1,12 +1,25 @@
 //check of spesific todos
-$("li").on("click", function(){
+
+//$("li").on("click", function(){ - will not work with appended li obj
+//second ardument - when li clicked inside ul
+//because ul already loaded, while potential li are not but listeners working once only
+$("ul").on("click","li", function(){ //will work with all potential and current li
   $(this).toggleClass("completed");
 });
 //delete icon
-$("span").on("click", function(event){
+$("ul").on("click","span", function(event){
   //prevent event bubbling with event
-  $(this).parent().fadeOut(1000,function(){
+  $(this).parent().fadeOut(800,function(){
     $(this).remove();
   });
   event.stopPropagation();
+});
+$("input[type='text']").keypress(function(event){
+  if(event.which===13){
+    var newTask = $(this).val();
+    $(this).val("");
+    //create new li and add to ul
+    $("ul").append("<li><span>X</span> "+newTask+"</li>");
+  }
+
 });
