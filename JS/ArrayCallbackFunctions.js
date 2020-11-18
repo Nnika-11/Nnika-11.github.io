e4a7b8fdaa84ae32b9b9aec731978f135ff4cc3f
Nnika-11 <sherr.veronika@gmail.com>
2020-11-18 16:01:12 -0500

    advanced callback

--- JS/ArrayCallbackFunctions.js
+++ JS/ArrayCallbackFunctions.js
@@ -27,6 +27,8 @@ const cars2 = cars.map(function(car){
   return "We have " +car.type;
 });
 
+
+
 //remember the arrow functions instead function expressiom
 const cars3 = cars.map(car => {
   return "We have " +car.type;
@@ -34,8 +36,36 @@ const cars3 = cars.map(car => {
 
 //use implicit returns - without return key word
 //if return 1 thing chnge curly braces to parenthesis
-const cars3 = cars.map(car => (
-  "We have " +car.type;
+const cars4 = cars.map(car => (
+  "We have " +car.type
 ));
 //or without
-const cars4 = cars.map(car => "We have " +car.type);
+const cars5 = cars.map(car => "We have " +car.type);
+
+// Filter - creates a new array with all elements that pass the test implemented by the provided function
+const result = cars.filter(car => car.capacity > 5).map(car => car.type);
+
+//every -  tests whether all elements in the array pass the test implemented by the provided function
+//some() method tests whether at least one element
+console.log(cars.every(car => car.type&&car.capacity&&car.color)) ;
+
+// reduce(accumulator, currentVal) - executes a reducer function on each element of the array, resulting in single output value.
+
+const sixFactorial = [2,3,4,5,6].reduce((accumulator,currentVal)=>{
+  return accumulator*currentVal;
+})
+
+var minValue = (min, cur) => min<cur ? min:cur;
+console.log([2,3,4,-1,5,6].reduce(minValue));
+console.log([2,3,4,-1,6,7].reduce(minValue,-8)); //add new val to score
+
+//setTimeout(CallBack, millisec))
+setTimeout(() => console.log("hello!"),3000);
+
+// setInterval(() => console.log(Math.random()),3000);
+// want to stop interval? - assign it to var
+// const id = setInterval(() => console.log(Math.random()),3000);
+//to stop:   clearInterval(id)
+
+
+//every
