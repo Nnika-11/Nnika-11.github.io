576afb23bf32073bf6257937cfdbc468203142ad
Nnika-11 <sherr.veronika@gmail.com>
2021-03-08 19:15:23 -0800

    finished with errors

--- JS/ScoreKeeper/ScoreKeeper.js
+++ JS/ScoreKeeper/ScoreKeeper.js
@@ -1,28 +1,50 @@
-p1 = document.querySelectorAll(".player1");
-p2 = document.querySelectorAll(".player2");
+ps1 = document.querySelectorAll(".player1");
+ps2 = document.querySelectorAll(".player2");
+p1 = document.querySelector("#p1");
+p2 = document.querySelector("#p2");
 p1Score = document.querySelector("#p1Score");
 p2Score = document.querySelector("#p2Score");
 maxValue = document.querySelector("#maxValue");
 
-setScore(p1,p1Score);
-setScore(p2,p2Score);
+  setScore(ps1, p1Score, p1);
+  setScore(ps2, p2Score, p2);
 
 
 
-function setScore(player, score){
-player.forEach((item) => {
-  item.addEventListener("click", ()=>{
-    score.innerText = parseInt(score.innerText)+1;
-    
-  })
-});
+function setScore(players, playerScore, player) {
+
+
+  players.forEach((item) => {
+    item.removeEventListener("click");
+    score = parseInt(playerScore.innerText);
+    item.addEventListener("click", () => {
+      score ++;
+      if (score == parseInt(maxValue.value)) {
+        player.classList.add("winner");
+      }
+      if (score > parseInt(maxValue.value));
+      else
+        playerScore.innerText = score;
+    })
+  });
 }
 
+reset = document.querySelector(".btn");
+reset.addEventListener("click", () =>{
+  p1.classList.remove("winner");
+  p2.classList.remove("winner");
+  p1Score.innerText = 0;
+  p2Score.innerText = 0;
+  maxValue.value = 10;
+  setScore(ps1, p1Score, p1);
+  setScore(ps2, p2Score, p2);
+});
+
 //exception handling
-maxValue.addEventListener("click",()=>{
+maxValue.addEventListener("click", () => {
   maxValue.value = "";
 });
-maxValue.addEventListener("mouseout",()=>{
-  if(maxValue.value<=0||!parseInt(maxValue.value))
+maxValue.addEventListener("mouseout", () => {
+  if (maxValue.value <= 0 || !parseInt(maxValue.value))
     maxValue.value = "10"
 });
