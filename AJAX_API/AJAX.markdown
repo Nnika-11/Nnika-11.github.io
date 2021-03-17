bf3962a0661e76e16a8441f543e916d74275da67
Nnika-11 <sherr.veronika@gmail.com>
2021-03-17 11:29:46 -0700

    Axios API

--- AJAX_API/AJAX.markdown
+++ AJAX_API/AJAX.markdown
@@ -23,3 +23,19 @@ fetch('http://example.com/movies.json')
   .then(response => response.json())
   .then(data => console.log(data));
 ```
+**NOTE:** You can't use try catch on fetch because fetch is async while try catch is sync. Therefore your try catch will always pass. Turn your function into an async and use await (or use .then &.catch)
+
+[Axios Code](https://github.com/axios/axios) (JSON Placeholder API)
+*Need to be installed*
+
+add header
+```
+const header = {headers: {Accept: 'application/json'}};
+axios.get('https://icanhazdadjoke.com/', header)
+  .then(result => document.getElementById("joke").innerText = result.data.joke)
+  .catch(error => alert("no jokes available. Sorry"))
+```
+
+URL: /search/shows?q=:query : - means API need some i from you
+q=:query - query string
+Example: http://api.tvmaze.com/search/shows?q=girls&embed=episodes
--- AJAX_API/Ajax.js
+++ AJAX_API/Ajax.js
@@ -1,6 +1,15 @@
-
-
-let json = {"ticker":{"base":"BTC","target":"USD","price":"443.7807865468","volume":"31720.1493969300","change":"0.3766203596"},"timestamp":1399490941,"success":true,"error":""};
+let json = {
+  "ticker": {
+    "base": "BTC",
+    "target": "USD",
+    "price": "443.7807865468",
+    "volume": "31720.1493969300",
+    "change": "0.3766203596"
+  },
+  "timestamp": 1399490941,
+  "success": true,
+  "error": ""
+};
 
 
 // JSON.parse(data) - data becomes a JavaScript object
@@ -10,15 +19,29 @@ let jsonObj = JSON.parse(jsonString);
 //Convert a JavaScript object into a string
 let newString = JSON.stringify(jsonObj);
 
-//URL: /search/shows?q=:query : - means API need some i from you
-//q=:query - query string
-//Example: http://api.tvmaze.com/search/shows?q=girls&embed=episodes
-
-
 // Fetch
 
-(async ()=>{
+(async () => {
   let response = await fetch('https://api.cryptonator.com/api/ticker/btc-usd');
   let jsonBody = await response.json();
   console.log("price for BTC is " + jsonBody.ticker.price);
+})();
+
+//Axios
+
+(() => {
+  axios.get('https://api.cryptonator.com/api/ticker/btc-usd')
+    .then(result => console.log("Price for BTC is " + result.data.ticker.price))
+})();
+
+
+(() => {
+  const header = {
+    headers: {
+      Accept: 'application/json'
+    }
+  };
+  axios.get('https://icanhazdadjoke.com/', header)
+    .then(result => document.getElementById("joke").innerText = result.data.joke)
+    .catch(error => alert("no jokes available. Sorry"))
 })();
